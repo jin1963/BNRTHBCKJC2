b20dd1fff94a95aca0784124989d09968a83be3a
KAOJNO DRAGON CHAIN <kaojino89@gmail.com>
2026-02-11 22:24:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// ====== CONFIG (BNB Smart Chain) ======
+window.APP_CONFIG = {
+  CHAIN_ID_DEC: 56,
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_NAME: "BNB Smart Chain",
+  RPC_URL: "https://bsc-dataseed.binance.org/",
+  BLOCK_EXPLORER: "https://bscscan.com",
+
+  // Contracts
+  CORE:     "0x66425FB7dAD286086b9fF024bf1e92A41926A50e", // CoreV4
+  EARNINGS: "0xf4e58b87909c68a07327ea3c82450D2Db51e6f0C", // EarningsV2
+  STAKE365: "0x575B29195ee74bcdAB538Ab4464BabADA13E24DA", // Stake365
+  VAULT:    "0xF703d77075976c5F4FB9ac264e5351a5D301c425", // Vault
+
+  // Tokens
+  USDT: "0x55d398326f99059fF775485246999027B3197955",
+
+  // Referral link
+  REF_PARAM: "ref",
+  SIDE_PARAM: "side", // L/R
+};
